c8042cce7adeb67a7f6f1546421be8adfefd98cc
pedro <pedroneves2906@hotmail.com>
2024-03-23 01:27:31 -0300

    criando a interface

--- Services/BrazilTaxService.cs
+++ Services/BrazilTaxService.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace locadora.Services
+﻿namespace locadora.Services
 {
-    internal class BrazilTaxService
+    class BrazilTaxService : ITaxService
     {
         public double Tax(double amount)
         {
--- obj/Debug/net8.0/locadora.AssemblyInfo.cs
+++ obj/Debug/net8.0/locadora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("locadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e79e5bf32c5eaaf29807e2ef462908049f9021d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("locadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("locadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
